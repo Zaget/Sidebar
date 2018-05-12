7f119c4d62c551c30e2df1e4027130c7b522df32
Mr. Mac <mrmac@Mrs-MacBook-Pro.local>
2018-05-11 19:06:06 -0700

    finisheddb

--- tests/db_tests/db.test.js
+++ tests/db_tests/db.test.js
@@ -1,33 +1,83 @@
-const db = require('../../database/index.js');
-
-const place_id = 'ChIJIbtvA4qAhYARdnwerOi-f4I';
+const q = require('../../database/SeedWithPostGres');
 
 
-test('the full dataset is a JSON array', () => {
-  expect.assertions(1);
-  return db.find().then((data) => {
-    expect(Array.isArray(data)).toBe(true);
-  });
+
+test('Random Function exists', () => {
+  expect(q.randomFunction).toBeTruthy();
+})
+
+test('Random Function Creates an Id with an input', () => {
+  let testNumber = 10;
+  let pie = q.randomFunction(testNumber);
+  expect(pie.id).toEqual(testNumber);
+})
+
+test('Random Function A random s', () => {
+  let testNumber = 10;
+  let pie = q.randomFunction(testNumber);
+  expect(pie.id).toEqual(testNumber);
+})
+
+test('Random Function Creates A Random Name', () => {
+  let testNumber1 = 10;
+  let testNumber2 = 11;
+  let FirstRandom = q.randomFunction(testNumber1)
+  let SecondRandom = q.randomFunction(testNumber2);
+  expect(typeof FirstRandom.name).toBe('string')
+  expect(FirstRandom.name).not.toBe(SecondRandom.name);
+})
+
+test('Random Function Creates Random Closing and Opening Times', () => {
+  let testNumber1 = 10;
+  let testNumber2 = 11;
+  let FirstRandom = q.randomFunction(testNumber1)
+  let SecondRandom = q.randomFunction(testNumber2);
+  expect(typeof FirstRandom.monday).toBe('string')
+  expect(FirstRandom.monday).not.toBe(SecondRandom.monday);
+})
+
+test('Random Function Creates Random Address', () => {
+  let testNumber1 = 10;
+  let testNumber2 = 11;
+  let FirstRandom = q.randomFunction(testNumber1)
+  let SecondRandom = q.randomFunction(testNumber2);
+  expect(typeof FirstRandom.address).toBe('string')
+  expect(FirstRandom.address).not.toBe(SecondRandom.address);
+})
+
+
+test('Random Function Creates A Random url', () => {
+  let testNumber1 = 10;
+  let testNumber2 = 11;
+  let FirstRandom = q.randomFunction(testNumber1)
+  let SecondRandom = q.randomFunction(testNumber2);
+  expect(typeof FirstRandom.url).toBe('string')
+  expect(FirstRandom.url).not.toBe(SecondRandom.url);
+})
+
+test('Random Function Creates A Random Phone', () => {
+  let testNumber1 = 10;
+  let testNumber2 = 11;
+  let FirstRandom = q.randomFunction(testNumber1)
+  let SecondRandom = q.randomFunction(testNumber2);
+  expect(typeof FirstRandom.phone).toBe('string')
+  expect(FirstRandom.phone).not.toBe(SecondRandom.phone);
+})
+
+test('Random Function Creates A Random Latitutde', () => {
+  let testNumber1 = 10;
+  let testNumber2 = 11;
+  let FirstRandom = q.randomFunction(testNumber1)
+  let SecondRandom = q.randomFunction(testNumber2);
+  expect(typeof FirstRandom.lat).toBe('string')
+  expect(FirstRandom.lat).not.toBe(SecondRandom.lat);
+})
+
+test('Random Function Creates A Random Longitude', () => {
+  let testNumber1 = 10;
+  let testNumber2 = 11;
+  let FirstRandom = q.randomFunction(testNumber1)
+  let SecondRandom = q.randomFunction(testNumber2);
+  expect(typeof FirstRandom.lng).toBe('string')
+  expect(FirstRandom.lng).not.toBe(SecondRandom.lng);
 });
-
-test('the response data contains an array for the place hours', () => {
-  expect.assertions(1);
-  return db.findOne({ id: place_id }).then((data) => {
-    expect(Array.isArray(data.hours)).toBe(true);
-  });
-});
-
-test('the response data contains an array of daily hours of length 7', () => {
-  expect.assertions(1);
-  return db.findOne({ id: place_id }).then((data) => {
-    expect(data.hours.length).toBe(7);
-  });
-});
-
-test('the response data contains the place name', () => {
-  expect.assertions(1);
-  return db.findOne({ id: place_id }).then((data) => {
-    expect(!!data.name).toBe(true);
-  });
-});
-
