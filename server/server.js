3ef34c8267bb319afa2900b5724682ebb703385b
Mr. Mac <mrmac@Mrs-MacBook-Pro.local>
2018-05-11 19:24:22 -0700

    FinishedServer

--- server/server.js
+++ server/server.js
@@ -1,40 +1,31 @@
-const express = require('express');
-// const morgan = require('morgan');
-const path = require('path');
+import nr from 'newrelic';
+import express from 'express';
+import React from 'react';
+import { renderToString } from 'react-dom/server';
+import templateFn from './template';
+import Sidebar from '../client/src/components/Sidebar.jsx';
+
 
 const app = express();
 
-const port = process.env.PORT || 3001;
-const bodyParser = require('body-parser');
+const port = const port = process.env.PORT || 3001;;
+const db = require('../database/indexPostGres');
 
+db.connect();
 
-const Places = require('../database/index.js');
-
-
-// app.use(morgan('dev'));
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  next();
-});
-
-app.use('/restaurants', express.static(path.join(__dirname, '../client/dist')));
+app.use(express.static('/Users/mrmac/ericCartman/sidebar-master/client/dist/'));
 
 app.get('/restaurants/:id', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/dist/index.html'));
-});
-
-app.get('/api/restaurants/:id', (req, res) => {
-  const q = Places.findOne({ id: req.params.id });
-
-  q.exec((err, place) => {
-    if (err) { console.log(err); }
-    // console.log('PLACE: ', place);
-    res.send(place);
+  res.header('Access-Control-Allow-Origin', '*');
+  db.any(`select * from zagnar where id = ${req.params.id}`).then((data) => {
+    const body = renderToString(<Sidebar data = {data} />);
+    const title = 'Zagetti';
+    res.send(templateFn(
+      title,
+      body,
+      data[0],
+    ));
   });
 });
 
-app.listen(port, () => {
-  // console.log(`server running at: http://localhost:${port}`);
-});
+app.listen(port);
