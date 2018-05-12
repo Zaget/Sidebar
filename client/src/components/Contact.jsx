15c19cf370183c3a4213e328e6631e0c5cfbe885
Mr. Mac <mrmac@Mrs-MacBook-Pro.local>
2018-05-11 19:46:44 -0700

    serversideComponents

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -1,36 +1,53 @@
 import React from 'react';
-import { Icon, Modal } from 'react-materialize';
+import { Icon } from 'react-materialize';
+import Modal from 'react-modal';
 
-export default class Contact extends React.Component {
-  // constructor(props) {
-  //   super(props);
-  // };
+const customStyles = {
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+  },
+};
+
+
+class Contact extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      modalIsOpen: false,
+    };
+
+    this.openModal = this.openModal.bind(this);
+    this.afterOpenModal = this.afterOpenModal.bind(this);
+    this.closeModal = this.closeModal.bind(this);
+  }
+
+  openModal() {
+    this.setState({ modalIsOpen: true });
+  }
+
+  afterOpenModal() {
+  // references are now sync'd and can be accessed.
+    this.subtitle.style.color = '#f00';
+  }
+
+  closeModal() {
+    this.setState({ modalIsOpen: false });
+  }
 
   render() {
-    let tel = `tel:${this.props.phone}`;
-    let store = this.props.name.split(' ').join('+');
-    let navLink = "https://www.google.com";
-    // `https://www.google.com/maps/dir/?api=1&destination=${store}&destination_place_id=${this.props.id}`;
-    // let modalLink= `https://www.google.com/maps/embed/v1/place?key=AIzaSyDfVc7Wl0xQOdHnSm30Yk2lZtcBTPEZtjM&q=place_id:${this.props.id}`;
+    const tel = `tel:${this.props.phone}`;
+    const store = this.props.name.split(' ').join('+');
+    const navLink = 'https://www.google.com';
+    const modalLink = `https://www.google.com/maps/embed/v1/view?key=AIzaSyCCkDYJwmT1_aBIYFxjGPHgw1hKI9Jjk4I&center=${this.props.lat},${this.props.lng}&maptype=satellite`;
     return (
+
       <div className="contact" style={{ content: { left: '0px', 'margin-left': '0px' } }}>
-        <Modal
-          className="mapModal"
-          trigger={<div className="address row">
-            <Icon className="contactIcon icons">location_on</Icon>
-            <span className="contactText">{this.props.address}</span>
-          </div>}
-        >
-          <iframe
-            width="1000px"
-            height="500px"
-            frameBorder="0" 
-            style={{ border: 0 }}
-            // src={modalLink} 
-            allowFullScreen
-          >
-          </iframe>
-        </Modal>
         <div className="phone row">
           <a href={tel} target="blank">
             <Icon className="contactIcon icons">phone</Icon>
@@ -38,10 +55,10 @@ export default class Contact extends React.Component {
           </a>
         </div>
         <div className="website row">
-          {/* <a href={this.props.website} target="blank">
+          <a href={this.props.website} target="blank">
             <Icon className="contactIcon icons">web</Icon>
             <span className="contactText">{this.props.website}</span>
-          </a> */}
+          </a>
         </div>
         <div className="directions row">
           <a href={navLink} target="blank">
@@ -49,9 +66,28 @@ export default class Contact extends React.Component {
             <span className="contactText">Get Directions</span>
           </a>
         </div>
+        <button onClick={this.openModal}>Enlarge Map</button>
+        <Modal
+          isOpen={this.state.modalIsOpen}
+          onAfterOpen={this.afterOpenModal}
+          onRequestClose={this.closeModal}
+          style={customStyles}
+          contentLabel="Example Modal"
+        >
+
+          <h2 ref={subtitle => this.subtitle = subtitle}>Click outside the box to leave!</h2>
+          <iframe
+            width="1000px"
+            height="500px"
+            frameBorder="0"
+            style={{ border: 0 }}
+            src={modalLink}
+            allowFullScreen
+          />
+        </Modal>
       </div>
     );
   }
 }
 
-window.Contact = Contact;
+export default Contact;
